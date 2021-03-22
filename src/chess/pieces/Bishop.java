d5bf4628db5d10c73e43e4a519f5d1ff853c52e5
Tiago Ssa <tiago.silva301@gmail.com>
2021-03-22 11:01:59 +0000

    Knight

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -22,7 +22,7 @@ public class Bishop extends ChessPiece {
 		
 		Position p = new Position(0, 0);
 		
-		//nothwest
+		//northwest
 		p.setValues(position.getRow() - 1, position.getColumn() - 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
@@ -32,7 +32,7 @@ public class Bishop extends ChessPiece {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 		
-		//notheast
+		//northeast
 		p.setValues(position.getRow() - 1, position.getColumn() + 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
